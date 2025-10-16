c6da37532da274523c0998a3b69901da165e8f82
Sahar Alwakily <33603273+Sahar-Alwakily@users.noreply.github.com>
2025-10-16 12:25:09 +0300

    asa

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
 const root = createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
