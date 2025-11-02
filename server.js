e58d392f3f756a07a49365c50b19ba508dfa64a4
Sahar Alwakily <33603273+Sahar-Alwakily@users.noreply.github.com>
2025-11-02 16:42:44 +0200

    gf

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// الاستماع على المنفذ الذي يعطيه Render أو 3000 افتراضي
 const PORT = process.env.PORT || 3000;
 
 // تقديم ملفات build
 app.use(express.static(path.join(__dirname, "build")));
 
-// كل الطلبات ترجع index.html (ضروري للـ React Router)
-app.get("*", (req, res) => {
+// أي مسار غير معروف يرسل index.html
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
